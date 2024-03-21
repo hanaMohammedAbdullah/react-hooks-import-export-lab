87b6b949fe10765b6f31ab683ab2b93712309c77
hanaMohammedAbdullah <hana.muhammed.abdulla@gmail.com>
2024-03-21 05:08:38 +0300

    Completed Assigment

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-function About() {
+function About({ image }) {
   return (
-    <div id="about">
+    <div id='about'>
       <h2>About Me</h2>
       <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <img src={image} alt='I made this' />
     </div>
   );
 }
+
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
+import { city, image, username } from "../data/user";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username={username} city={city} />
+      <About image={image} />
     </div>
   );
 }
